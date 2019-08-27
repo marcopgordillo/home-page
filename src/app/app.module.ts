d9a09774364f4b79c91c7e2be2812a6296ab2768
Marco Gordillo <6589245+marcopgordillo@users.noreply.github.com>
2019-08-27 13:00:10 -0400

    updated project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
-import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {NavbarComponent} from './navbar/navbar.component';
@@ -10,7 +9,7 @@ import {WelcomeComponent} from './welcome/welcome.component';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {library} from '@fortawesome/fontawesome-svg-core';
 import {faGlobe, faMapMarkerAlt, faPhone} from '@fortawesome/free-solid-svg-icons';
-import {faEnvelope, faSmileBeam, faSmileWink} from '@fortawesome/free-regular-svg-icons';
+import {faEnvelope, faSmileBeam, faSmileWink, faBookmark} from '@fortawesome/free-regular-svg-icons';
 import {faFacebookF, faGithub, faInstagram, faLinkedinIn, faTwitter} from '@fortawesome/free-brands-svg-icons';
 import {SocialMenuComponent} from './social-menu/social-menu.component';
 import {AboutComponent} from './about/about.component';
@@ -29,14 +28,13 @@ import {SkillsComponent} from './skills/skills.component';
     BrowserModule,
     BrowserAnimationsModule,
     CollapseModule.forRoot(),
-    FontAwesomeModule,
-    AppRoutingModule
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
   constructor() {
-    library.add(faSmileWink, faSmileBeam, faFacebookF, faTwitter, faLinkedinIn, faInstagram, faGithub, faMapMarkerAlt, faGlobe, faEnvelope, faPhone);
+    library.add(faSmileWink, faSmileBeam, faFacebookF, faTwitter, faLinkedinIn, faInstagram, faGithub, faMapMarkerAlt, faGlobe, faEnvelope, faPhone, faBookmark);
   }
 }
